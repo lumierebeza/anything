66386acb3b72d3c620b0ea6dc68dbf89d712f550
lumiere.beza <bezawit.tesera@seattlecolleges.edu>
2024-06-05 12:43:53 -0700

    sixth commit

--- bye.py
+++ bye.py
@@ -1,54 +1,58 @@
 import discord
 import os
+import random 
 from dotenv import load_dotenv
 from ec2_metadata import ec2_metadata
 
-load_dotenv()
 
-# Initialize the Discord bot with the necessary intents
+load_dotenv() 
+
+# Initialize the Discord bot
 intents = discord.Intents.default()
 intents.messages = True
-
-# Check if message_content intent is available
-if hasattr(intents, 'message_content'):
-    intents.message_content = True
+intents.message_content = True
 
 client = discord.Client(intents=intents)
 
 # Get the token from the environment variables
-token = os.getenv('TOKEN')
+token = str(os.getenv('TOKEN'))
 
-print('This is my EC2 metadata region:', ec2_metadata.region)
-print('This is my EC2 metadata instance ID:', ec2_metadata.instance_id)
+print('This is my Ec2_metadata.region:', ec2_metadata.region)
+print('This is my Ec2_metadata.instance.id:', ec2_metadata.instance_id)
 
-@client.event
-async def on_ready():
+
+@client.event 
+async def on_ready(): 
     # Print a message when the bot is logged in and ready
-    print(f"Logged in as {client.user}")
+    print("Logged in as a bot {0.user}".format(client))
 
-@client.event
-async def on_message(message):
+@client.event 
+async def on_message(message): 
     # Get the username, channel, and message content
-    username = str(message.author).split("#")[0]
-    channel = str(message.channel.name)
-    user_message = str(message.content)
+    username = str(message.author).split("#")[0] 
+    channel = str(message.channel.name) 
+    user_message = str(message.content) 
 
     # Log the message
-    print(f'Message "{user_message}" by {username} on {channel}')
+    print(f'Message {user_message} by {username} on {channel}') 
 
     # Prevent the bot from replying to itself
-    if message.author == client.user:
+    if message.author == client.user: 
         return
 
     # Check if the message is in the "random" channel
-    if channel == "random":
+    if channel == "random": 
         # Respond to greetings
-        if user_message.lower() == "bangtan?":
-            await message.channel.send(f'ARMY! {username} Your EC2 Data: {ec2_metadata.region}')
-        elif user_message.lower() == "hello":
-            await message.channel.send(f'Hello {username}')
-        elif user_message.lower() == "ec2 data":
-            await message.channel.send(f'Your instance data is: Region - {ec2_metadata.region}, Instance ID - {ec2_metadata.instance_id}')
+        if user_message.lower() == "Bangtan?":
+            await message.channel.send(f'ARMY! {username} Your EC2 Data: {ec2_metadata.region}') 
+            return
+        # other string options
+        elif user_message.lower() == "Hello": 
+            await message.channel.send(f'Hello {username}') 
+
+        # Returning instance data for the last conditional statement.
+        elif user_message.lower() == "EC2 Data": 
+            await message.channel.send("Your instance data is " + ec2_metadata) 
 
 # Run the bot with the token
 client.run(token)
